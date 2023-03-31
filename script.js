d0db36f0081c32b06880de697b1d4839f59a13c9
percy chuzon <percyrondan1995@gmail.com>
2023-03-30 22:24:41 -0500

    se agrego funcion anonima

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let xylophone = document.querySelector(".contenedor");
+(() => {
+  let xylophone = document.querySelector(".contenedor");
 
-xylophone.addEventListener("click", (e) => {
-  let sound = new Audio(`sound/${e.target.id}.mp3`);
-  const tocar = (sounds) => {
-    sounds.pause();
-    sounds.currentTime = 0;
-    sounds.play();
-    e.target.classList.add("sombra");
-    setTimeout(() => {
-      e.target.classList.remove("sombra");
-    }, 300);
-  };
-  if (e.target.classList.contains("note")) {
-    tocar(sound);
-  }
-});
+  xylophone.addEventListener("click", (e) => {
+    let sound = new Audio(`sound/${e.target.id}.mp3`);
+    const tocar = (sounds) => {
+      sounds.pause();
+      sounds.currentTime = 0;
+      sounds.play();
+      e.target.classList.add("sombra");
+      setTimeout(() => {
+        e.target.classList.remove("sombra");
+      }, 300);
+    };
+    if (e.target.classList.contains("note")) {
+      tocar(sound);
+    }
+  });
+})();
